1ff7e31c247f85fa4ded972ffd7c09c6b3522250
skeletona <skeletona@github.com>
2025-01-25 20:19:56 +0300

    fix prepare.sh

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,8 @@ WORKDIR /home
 
 COPY ./docker-compose /usr/lib/docker/cli-plugins/docker-compose
 COPY ./services /home/services
+COPY ./motd /etc/motd
 COPY ./tmp /tmp
 
-RUN dockerd & sleep 2 && fish /tmp/prepare.fish
+RUN dockerd & sleep 2 && /tmp/prepare.sh
 CMD /tmp/entrypoint.sh
--- tmp/prepare.sh
+++ tmp/prepare.sh
@@ -7,28 +7,11 @@ hostname player
 echo "alias ip \"ip -c=auto\"
 cd /home" > /etc/fish/config.fish
 fish -c 'set -U fish_greeting ""'
-echo "           wake up, Neo...
-        the matrix has you 
-      follow the white rabbit.
 
-          knock, knock, Neo.
-
-                        (`.         ,-,
-                        ` `.    ,;' /
-                         `.  ,'/ .'
-                          `. X /.'
-                .-;--''--.._` ` (
-              .'            /   `
-             ,           ` '   Q '
-             ,         ,   `._    \\
-          ,.|         '     `-.;_'
-          :  . `  ;    `  ` --,.._;
-           ' `    ,   )   .'
-              `._ ,  '   /_
-                 ; ,''-,;' ``-
-                  ``-..__``--`" > /etc/motd
-docker load -i /tmp/images.tar
-rm -f /tmp/images.tar
+if [ -f /tmp/images.tar ]; then
+	docker load -i /tmp/images.tar
+	rm -f /tmp/images.tar
+fi
 
 for service in /home/services/*; do
 	docker compose -f $service/docker-compose.yml create;
