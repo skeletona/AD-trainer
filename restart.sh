af9f17b065ff85db989e6392429574c5b6ec1b95
skeletona <skeletona@github.com>
2025-01-28 17:42:04 +0300

    update

--- restart.sh
+++ restart.sh
@@ -2,19 +2,32 @@
 
 R=$(tput setaf 1)
 G=$(tput setaf 2)
-B=$(tput setaf 4)
+B=$(tput setaf 3)
 Z=$(tput sgr0)
 
-echo "${R}Stopping containers...${Z}"
-docker stop -t 0 player game forcad
+if [ ! -f ForcAD/images.tar ]; then
+	echo "${B}Looks like ForcAD is not built! Building Forcad ^_^${Z}"
+	cd ForcAD/ForcAD
+	python3 -m venv ./
+	./bin/pip install -r requirements.txt
+	./bin/python control.py setup
+	docker compose pull
+	docker compose build
+	docker save -o ../images.tar forcad-flower forcad-celery forcad-events forcad-admin-api forcad-http-receiver forcad-client-api forcad-ticker forcad-initializer forcad-nginx postgres rabbitmq redis
+	cd ../../
+	echo "${G}ForcAD built!${Z}"
+fi
 
 echo "${R}Deleting containers...${Z}"
-docker rm player game forcad
+docker rm --force forcad player vpn
 
-echo "${R}Deleting image...${Z}"
-docker rmi ad-trainer-player forcad | grep -v "sha256"
+echo "${R}Deleting network...${Z}"
+docker network rm game
 
-echo "${B}Starting container...${R}"
+echo "${G}Starting simulation...${R}"
 docker compose up -d --build
 
-echo "${G}Go to http://62.173.146.31:8000 to get your OVPN config!${Z}"
+echo "${R}Deleting unused images${Z}"
+docker rmi $(docker images -f "dangling=true" -q) | grep -v "sha256"
+
+echo "${G}ALL DONE! Go to http://62.173.146.31:8000 to get your OVPN config!${Z}"
